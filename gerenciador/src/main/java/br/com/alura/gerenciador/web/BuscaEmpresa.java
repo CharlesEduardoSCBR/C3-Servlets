a5585b2014aa05b9851088a06391dd9164dbb856
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-30 22:32:39 -0200

    C3M1_6-Executando-uma-busca

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -2,12 +2,16 @@ package br.com.alura.gerenciador.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.Empresa;
+import br.com.alura.gerenciador.dao.EmpresaDAO;
 
 @WebServlet(urlPatterns="/busca")
 public class BuscaEmpresa extends HttpServlet {
@@ -21,6 +25,14 @@ public class BuscaEmpresa extends HttpServlet {
 		writer.println("<html>");
 		writer.println("<body>");
 		writer.println("Resultado da busca:<br/>");
+		
+		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade("doce");
+		
+		writer.println("<ul>");
+		for(Empresa empresa : empresas){
+			writer.println("<li>" + empresa.getId() + ": " + empresa.getNome() + "</li>");
+		}
+		writer.println("</ul>");
 		writer.println("</body>");
 		writer.println("</html>");
 	}
