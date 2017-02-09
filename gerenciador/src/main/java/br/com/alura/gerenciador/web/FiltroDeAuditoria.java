fe25adca3cee788539d8043406b4ea07ebe40d01
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-02-09 20:39:45 -0200

    C3M5_3-Login

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @WebFilter(urlPatterns = "/*")
@@ -17,7 +18,26 @@ public class FiltroDeAuditoria implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
+		Cookie cookie = getUsuario(req);
+		String usuario = "<deslogado>";
+		
+		if(cookie != null)
+			usuario = cookie.getValue();
+
 		System.out.println("Usuario acessando a URI " + req.getRequestURI());
 		chain.doFilter(request, response);
+	}
+
+	private Cookie getUsuario(HttpServletRequest req) {
+		Cookie[] cookies = req.getCookies();
+
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals("usuario.logado")) {
+				if (cookie.getName().equals("usuario.logado"))
+					;
+				return cookie;
+			}
+		}
+		return null;
 	}
 }
