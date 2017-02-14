497cae31bcdf2d8019ab9a270058f16e412d5029
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-02-14 13:50:44 -0200

    C3M7_4_2

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -17,7 +17,6 @@ import br.com.alura.gerenciador.dao.EmpresaDAO;
 public class BuscaEmpresa extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	String filtro;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -27,7 +26,7 @@ public class BuscaEmpresa extends HttpServlet {
 		writer.println("<body>");
 		writer.println("Resultado da busca:<br/>");
 
-		filtro = req.getParameter("filtro");
+		String filtro = req.getParameter("filtro");
 
 		try {
 			Thread.sleep(10000);
