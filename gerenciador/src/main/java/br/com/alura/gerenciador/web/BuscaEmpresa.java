a71088bad0580f322dcfb6db270a52012ada422b
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-02-14 13:46:42 -0200

    C3M7_4-Ambiente_multithread_online

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -13,29 +13,37 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.alura.gerenciador.Empresa;
 import br.com.alura.gerenciador.dao.EmpresaDAO;
 
-@WebServlet(urlPatterns="/busca")
+@WebServlet(urlPatterns = "/busca")
 public class BuscaEmpresa extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+	String filtro;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 		PrintWriter writer = resp.getWriter();
 		writer.println("<html>");
 		writer.println("<body>");
 		writer.println("Resultado da busca:<br/>");
-		
-		String filtro = req.getParameter("filtro");
+
+		filtro = req.getParameter("filtro");
+
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filtro);
-		
+
 		writer.println("<ul>");
-		for(Empresa empresa : empresas){
+		for (Empresa empresa : empresas) {
 			writer.println("<li>" + empresa.getId() + ": " + empresa.getNome() + "</li>");
 		}
 		writer.println("</ul>");
 		writer.println("</body>");
 		writer.println("</html>");
 	}
-		
+
 }
