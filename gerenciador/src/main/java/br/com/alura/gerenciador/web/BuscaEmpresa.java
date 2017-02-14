b65304cc301d77df01f92c51ba3067c41abb5be8
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-02-14 15:02:13 -0200

    C3M11

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collection;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,29 +21,19 @@ public class BuscaEmpresa extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-		PrintWriter writer = resp.getWriter();
-		writer.println("<html>");
-		writer.println("<body>");
-		writer.println("Resultado da busca:<br/>");
-
 		String filtro = req.getParameter("filtro");
 
-		try {
+		/*try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}
+		}*/
 
 		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filtro);
-
-		writer.println("<ul>");
-		for (Empresa empresa : empresas) {
-			writer.println("<li>" + empresa.getId() + ": " + empresa.getNome() + "</li>");
-		}
-		writer.println("</ul>");
-		writer.println("</body>");
-		writer.println("</html>");
+		
+		req.setAttribute("empresas", empresas);
+		RequestDispatcher disp = req.getRequestDispatcher("WEB-INF/paginas/buscaEmpresa.jsp");
+		disp.forward(req, resp);
 	}
 
 }
